37501dc1278bbc32c0db81d92e541a793edf8e75
Devaraj Ranganna <devaraj.ranganna@arm.com>
2021-10-27 14:53:45 +0100

    samples: tf-m: Update sample to get sine values
    A secure service that uses tensorflow lite-micro framework to run
    inference on a pre-trained model is available. Update sample to get sine
    values from the secure service and print it onto console along with the
    deviation observed relative to sine value from C math library.
    
    Signed-off-by: Devaraj Ranganna <devaraj.ranganna@arm.com>

--- samples/tfm_integration/psa_custom_service/src/main.c
+++ samples/tfm_integration/psa_custom_service/src/main.c
@@ -12,6 +12,8 @@
 #include "util_app_log.h"
 #include "util_sformat.h"
 
+#include <math.h>
+
 /** Declare a reference to the application logging interface. */
 LOG_MODULE_DECLARE(app, CONFIG_LOG_DEFAULT_LEVEL);
 
@@ -29,6 +31,11 @@ void main(void)
 	size_t hash_len;
 	psa_status_t status;
 	uint8_t lsm303_data[6] = {0};
+
+	const float PI = 3.14159265359f;
+	float deg = PI/180.0;
+
+	float x_value, y_value;
 
 	/* Initialise the logger subsys and dump the current buffer. */
 	log_init();
@@ -58,26 +65,53 @@ void main(void)
 	/* Display the SHA-256 hash */
 	sf_hex_tabulate_16(&main_fmt, hash, (size_t)(PSA_HASH_SIZE(PSA_ALG_SHA_256)));
 
-	while(true) {
-		LOG_INF("Reading LSM303");
-		al_dump_log();
+	/* To read LSM303 values QEMU must be started with following additional parameters:
+		qemu-system-arm -M mps2-an521 -device loader,file=./build/tfm_merged.hex -serial stdio \
+			-monitor tcp:localhost:4444,server,nowait \
+			-device lsm303dlhc_mag,id=lsm303,address=0x1E
+	*/
+	LOG_INF("Reading LSM303");
+	al_dump_log();
 
+	status = al_psa_status(
+		example_read_lsm303(lsm303_data,
+							sizeof(lsm303_data)),
+		__func__);
+
+	if (status != PSA_SUCCESS) {
+		LOG_ERR("Failed to get values from LSM303");
+		goto err;
+	}
+
+	LOG_INF("LSM303 magnetometer values: ");
+	LOG_INF("mag_x: %x", (uint16_t)((lsm303_data[0] << 8) | lsm303_data[1]));
+	LOG_INF("mag_y: %x", (uint16_t)((lsm303_data[4] << 8) | lsm303_data[5]));
+	LOG_INF("mag_z: %x", (uint16_t)((lsm303_data[2] << 8) | lsm303_data[3]));
+
+	LOG_INF("Get sine value using secure inference");
+	al_dump_log();
+
+	for(int i = 0; i <=360; i++) {
+
+		x_value = (float)i * deg;
 		status = al_psa_status(
-			example_read_lsm303(lsm303_data,
-								sizeof(lsm303_data)),
-			__func__);
+			psa_example_tflm_hello(&x_value,
+									sizeof(x_value),
+									&y_value,
+									sizeof(y_value)),
+								__func__);
 
 		if (status != PSA_SUCCESS) {
-			LOG_ERR("Failed to get values from LSM303");
+			LOG_ERR("Failed to get sine value using secure inference");
 			goto err;
 		}
 
-		LOG_INF("LSM303 magnetometer values: ");
-		LOG_INF("mag_x: %x", (uint16_t)((lsm303_data[0] << 8) | lsm303_data[1]));
-		LOG_INF("mag_y: %x", (uint16_t)((lsm303_data[4] << 8) | lsm303_data[5]));
-		LOG_INF("mag_z: %x", (uint16_t)((lsm303_data[2] << 8) | lsm303_data[3]));
+		printf("Model: Sine of %d deg is: %f\t", i, y_value);
+		printf("C Mathlib: Sine of %d deg is: %f\t", i, sin(x_value));
+		printf("Deviation: %f\n", fabs(sin(x_value) - y_value));
+		al_dump_log();
 
-		k_msleep(2000);
+		k_msleep(500);
 	}
 
 err:
